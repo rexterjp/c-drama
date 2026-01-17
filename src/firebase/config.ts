1292fe396800347420761ccef325f0ca06d0da0c
tokomicin <candrapramudya01@gmail.com>
2026-01-17 09:02:35 +0000

    oke buat file env aku mau deploy

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,8 +1,8 @@
 export const firebaseConfig = {
-  "projectId": "studio-4125449844-94621",
-  "appId": "1:258898163010:web:479d2cbd791d0871bdc4db",
-  "apiKey": "AIzaSyCsQbYfrvUP8l8kwFXraF8v5lNrPAZWwGU",
-  "authDomain": "studio-4125449844-94621.firebaseapp.com",
-  "measurementId": "",
-  "messagingSenderId": "258898163010"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
